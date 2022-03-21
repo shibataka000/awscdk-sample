8169ad374ba84caea53881099f72e0d010548d49
Takao Shibata <chise.alter.pasta@gmail.com>
2022-03-21 12:23:53 +0900

    add hello-cdk

---
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+  }
+}
